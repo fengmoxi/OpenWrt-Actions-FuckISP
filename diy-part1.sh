4bc0166f3e67713afa109a287cf96b738abedcaa
风漠兮 <fengmoxi@vip.qq.com>
2026-02-04 12:37:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+git clone https://github.com/yingziwu/luci-app-fakehttp.git package/luci-app-fakehttp
+git clone https://github.com/yingziwu/openwrt-fakehttp.git package/openwrt-fakehttp
